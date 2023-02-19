4ec88adc146920fd52ff304f10e667de6186467f
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-02-19 22:06:26 +0700

    feat(init): init infrastructure

--- internal/infrastructure/infra.go
+++ internal/infrastructure/infra.go
@@ -1,1 +1,32 @@
 package infrastructure
+
+import "github.com/gofrs/uuid"
+
+type HashManager interface {
+	HashPassword(s string) ([]byte, error)
+}
+
+type UUIDManager interface {
+	GenerateUUID() (uuid.UUID, error)
+}
+
+type Infra struct {
+	hash HashManager
+	uuid UUIDManager
+}
+
+func New(hash HashManager, uuid UUIDManager) *Infra {
+	return &Infra{
+		hash: hash,
+		uuid: uuid,
+	}
+}
+
+func (infra *Infra) HashPassword(s string) (string, error) {
+	result, err := infra.hash.HashPassword(s)
+	return string(result), err
+}
+func (infra *Infra) GenerateUniqueID() (string, error) {
+	res, err := infra.uuid.GenerateUUID()
+	return res.String(), err
+}
