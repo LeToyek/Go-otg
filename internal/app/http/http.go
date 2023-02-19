044d8ebe5120c41502e0027c46c4cbf97a8c0fab
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-02-19 22:08:11 +0700

    feat(init): init db connection

--- internal/app/http/http.go
+++ internal/app/http/http.go
@@ -2,12 +2,16 @@ package http
 
 import (
 	// golang package
-	"context"
+
 	"fmt"
 	"go-otg/internal/app/http/server"
+	"go-otg/internal/infrastructure"
+	hash "go-otg/internal/infrastructure/hash"
+	"go-otg/internal/infrastructure/uuid"
 	"go-otg/internal/repository/db"
 	"go-otg/internal/repository/redis"
 	"go-otg/internal/server/http"
+
 	"os"
 
 	// external package
@@ -19,23 +23,34 @@ import (
 // NewApplication new application.
 func NewApplication() {
 
-	psqlInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"localhost",
+		"5432",
+		"postgres",
+		"handoko",
+		"go-otg")
+	if os.Getenv("ENV") == "PRODUCTION" {
+		psqlInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_OPEN_PORT"),
+			os.Getenv("DB_USERNAME"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"))
+	}
+
 	sqlDB, err := sqlx.Connect("postgres", psqlInfo)
 	if err != nil {
 		fmt.Print(err.Error())
 		fmt.Println("EMPTY DB CONNECTION STRING")
-		// log.Fatal(err)
 	}
 
 	client := redis.NewRedisClient(os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PASSWORD"))
 
 	repositoryRedis := redis.NewRedis(client)
 	repositoryDB := db.New(sqlDB)
-	// fmt.Print("Error Here")
-	fmt.Print(repositoryDB.GetUserByID(context.Background(), "asd"))
-	// fmt.Print(" <<- error")
+	infra := infrastructure.New(hash.NewHasher(), uuid.NewUUIDService())
 	resources := server.NewResources(repositoryDB, repositoryRedis)
-	services := server.NewServices(resources)
+	services := server.NewServices(infra, resources)
 	usecases := server.NewUsecases(services)
 	handlers := server.NewHandlers(usecases)
 	httpServer := http.NewHTTPServer(handlers)
