bbf53a19ef7119d6fdf69f8f4df2d1f74cdc036d
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-02-19 22:05:54 +0700

    feat(update): update migrations

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,8 +1,17 @@
 CREATE TABLE users(
 	ID VARCHAR(50) primary key,
-	username VARCHAR(25),
-	email VARCHAR(50),
-	password VARCHAR(50),
-	address VARCHAR(255),
-	age INT
+	username VARCHAR(25) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	age INT NOT NULL
+);
+
+INSERT INTO users VALUES(
+	'asd',
+	'asokdoask',
+	'oaskdok@gmail.com',
+	'aowkdoawkd',
+	'asodkoasdk',
+	4
 );
